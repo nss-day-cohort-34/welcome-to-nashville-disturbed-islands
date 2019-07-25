2df9a77dced28fc17e95722ccab464a5ee5a2412
Caroline Brownlee <brownleecaroline@gmail.com>
2019-07-25 15:46:02 -0500

    created initial files

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+// Park Section
+
+
+
+// End Park Section
+
+
+
+// Concert Section
+
+
+
+// End Concert Section
+
+
+
+// Restaurant Section
+
+
+
+// End Restaurant Section
+
+
+
+
+// MeetUp Section
+
+
+
+// End MeetUp Section
