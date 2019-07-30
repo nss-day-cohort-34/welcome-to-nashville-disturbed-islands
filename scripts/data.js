3d4d1a3b2fcbb08451d563ba7574ab464dd402f8
Caroline Brownlee <brownleecaroline@gmail.com>
2019-07-30 10:05:00 -0500

    changes

--- scripts/data.js
+++ scripts/data.js
@@ -13,14 +13,14 @@ const getParkByName = (parkName) => {
 
 
 
-Concert Section
+// Concert Section
 const concertData = (search) => {
     return fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=67yF0arVBeAHL7bl8nHGdz1e58DPQbjd&classificationName=music&dmaId=343&keyword=${search}`)
         .then(response => response.json())
         .then(data => console.log(data))
 }
 
-End Concert Section
+// End Concert Section
 
 
 
